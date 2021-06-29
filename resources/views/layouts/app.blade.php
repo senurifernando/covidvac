1e392ca2b52feaa99d36ab0552aae05a9f9e73aa
senurifernando <senuri0604@gmail.com>
2021-06-30 00:05:03 +0530

    todays commits 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,38 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
-    <title>Covid Vaccination Appointment System</title>
+    <title>COVID 19 VACCINATION RESERVATION</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
-<body>
-    <nav class="navbar navbar-light bg-light ">
-        <!--<span class="navbar-brand mb-0 h1">COVID 19 VACCINATION RESERVATION</span>-->
+<body class="bg-gray-300">
 
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                <a href="" class="p-3 ">Home</a>
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">LOGIN</a>
+                <a href="" class="p-3">General Guidelines</a>
             </li>
             <li>
-                <a href="" class="p-3">REGISTER</a>
+                <a href="" class="p-3">Vaccines</a>
             </li>
+        </ul>
 
+        <ul class="flex items-center">
+            <!--
+
+                        <li>
+                            <a href="" class="p-6">SENNA</a>
+                        </li>
+                        <li>
+                            <a href="" class="p-6">Logout</a>
+                        </li>-->
+
+
+
+            <li>
+                <a href="" class="p-6">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-6">Register</a>
+            </li>
 
         </ul>
+    </nav>
     </nav>
     @yield('content')
 </body>

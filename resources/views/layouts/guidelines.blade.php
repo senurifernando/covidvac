66351f0f33dfca358ea309c1e8e9cc8ea1d1014b
senurifernando <senuri0604@gmail.com>
2021-07-23 17:07:40 +0530

    commit 3

--- resources/views/layouts/guidelines.blade.php
+++ resources/views/layouts/guidelines.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
 
+    <div class="flex">
+
+        <ul class=" p-6 list-disc">
+            <h1>Before Vaccinated Make Sure You Have:</h1><br>
+            <li>A Mask that covers your nose and mouth- and fits tightly and comfortably</li>
+            <li>Hand Sanitizer</li>
+            <li>Your NIC Card</li>
+        </ul>
+    </div>
 @endsection

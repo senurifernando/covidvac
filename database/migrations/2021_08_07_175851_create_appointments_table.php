579a936c3b030994e7eddbf3716a876ea57dad19
senurifernando <senuri0604@gmail.com>
2021-08-09 14:00:39 +0530

    appointment form Created

--- database/migrations/2021_08_07_175851_create_appointments_table.php
+++ database/migrations/2021_08_07_175851_create_appointments_table.php
@@ -15,6 +15,7 @@ class CreateAppointmentsTable extends Migration
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('NIC');
             $table->string('province');
             $table->string('district');
@@ -23,7 +24,7 @@ class CreateAppointmentsTable extends Migration
             $table->timeTz('time', $precision = 0); //admin
             $table->string('location'); //admin
             $table->string('dosenum');
-            $table->string('phone', 10);
+            $table->integer('phone', 10);
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
@@ -31,7 +32,7 @@ class CreateAppointmentsTable extends Migration
         });
 
         Schema::table('appointments', function (Blueprint $table) {
-            $table->unsignedBigInteger('NIC');
+            $table->string('NIC');
             $table->foreign('NIC')->references('NIC')->on('users');
         });
     }
--- resources/views/layouts/appointmentForm.blade.php
+++ resources/views/layouts/appointmentForm.blade.php
@@ -1,1 +1,92 @@
-dgjslfgjfsgjlfsgfjsgf
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center md:flex">
+        <div class="w-4/12 bg-white p-6 rounded-lg m-5">
+            <form>
+                @csrf
+                <div class="mb-4">
+                    <label for="name" class="sr-only">name</label>
+                    <input type="text" name="name" id="name" placeholder="Enter your name With Initials"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500
+                                                                                                                                                            @enderror" value="{{ old('name') }}">
+                    @error('name')
+                        <div class="text-red-600 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+                <div class="mb-4">
+                    <label for="NIC" class="sr-only">NIC</label>
+                    <input type="text" name="NIC" id="NIC" placeholder="Enter your NIC number"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('NIC') border-red-500
+                                                                                                                                                            @enderror"
+                        value="{{ old('NIC') }}">
+                    @error('NIC')
+                        <div class="text-red-600 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+                <div class="mb-4">
+                    <label for="province" class="sr-only">province</label>
+                    <input type="text" name="province" id="province" placeholder="Enter your province"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('province') border-red-500
+                                                                                                                                                            @enderror" value="{{ old('province') }}">
+                    @error('province')
+                        <div class="text-red-600 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+                <div class="mb-4">
+                    <label for="district" class="sr-only">district</label>
+                    <input type="text" name="district" id="district" placeholder="Enter your district"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('district') border-red-500
+                                                                                                                                                    @enderror" value="{{ old('district') }}">
+                    @error('district')
+                        <div class="text-red-600 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+                <div class="mb-4">
+                    <label for="injection_name">Injection name: </label>
+                    <input type="text" name="injection_name" placeholder="selected injection" id="injection_name"
+                        class="bg-gray-100 border-2 w-full
+                                                                                                                    p-4 rounded-lg">
+                </div>
+                <div class="mb-4">
+                    <label for="dosenum">Dosage : </label>
+                    <input type="radio" name="dosenum" id="dosenum" class="form-radio" value="1st Dose">
+                    <span class="ml-2">1st Dose</span>
+                    <input type="radio" name="dosenum" id="dosenum" class="form-radio" value="2nd Dose">
+                    <span class="ml-2">2nd Dose</span>
+                </div>
+                <div class="mb-4">
+                    <label for="phone" class="sr-only">Phone :</label>
+                    <input type="text" name="phone" id="phone" placeholder="Enter your Phone number" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('phone') border-red-500
+                                                                    @enderror" value="{{ old('phone') }}">
+                    @error('phone')
+                        <div class="text-red-600 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="email">Email :</label>
+                    <input type="email" name="email" id="email" placeholder="email"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                </div>
+                <div>
+                    <button type="submit"
+                        class="bg-blue-400 text-white px-4 py-3 rounded font-medium w-full">SUBMIT</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection

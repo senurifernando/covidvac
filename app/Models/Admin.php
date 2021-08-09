8966e46c11177f970f4e986d9ee38f8105980046
senurifernando <senuri0604@gmail.com>
2021-08-09 11:52:18 +0530

    ------

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\CanResetPassword;
+//use Illuminate\Auth\Passwords\CanResetPassword;
 
 class Admin extends Authenticatable
 {
